90642521902f5dbea2c0cea25b6ab4b3e1d5d38e
Alessandro <alessandrovalori90@gmail.com>
2016-10-14 12:58:25 +0200

    testing thread lock

--- src/italo/Main.java
+++ src/italo/Main.java
@@ -3,6 +3,8 @@ package italo;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,7 +14,8 @@ import javafx.stage.Stage;
 public class Main extends Application{
 
 	public static void main(String[] args) throws IOException {
-		launch(args);
+		Application.launch();
+		//launch(args);
 	}
 	
 	@Override
--- src/italo/gui/MainWindowController.java
+++ src/italo/gui/MainWindowController.java
@@ -5,25 +5,30 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 
+import com.sun.javafx.application.PlatformImpl;
+
 import italo.core.GetPromo;
 import italo.core.Util;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
+import javafx.embed.swing.JFXPanel;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.util.Duration;
 
-public class MainWindowController implements Initializable {
+public class MainWindowController {
 	@FXML private ListView<String> valid;
 	@FXML private ListView<String> expired;
 	@FXML private ListView<String> unkown;
 	private Service<Void> backgroundThread;
-	Thread myTaskThread;
-	MyTask myTask;
 	
 	public void test(){
 		
@@ -51,8 +56,20 @@ public class MainWindowController implements Initializable {
 											end = util.add();
 										} else {
 											data.add(util.getUrl());
-											valid.setItems(data);	
-											//updateMessage(data.toString()); // non so perchè funziona al posto di 
+											//valid.setItems(data);	
+											//updateMessage(data.toString()); // non so perchè funziona al posto di
+											//Platform.runLater(() -> valid.accessibleTextProperty().set(data.toString()));
+											//Platform.runLater(() -> valid.setItems(data));
+											
+											cose(data);
+											/*
+											KeyFrame update = new KeyFrame(Duration.seconds(0.5), event -> { 
+												valid.setItems(data);
+											}); 
+											Timeline tl = new Timeline(update); 
+											tl.setCycleCount(Timeline.INDEFINITE);
+											tl.play();
+											*/
 										}
 									}
 								}
@@ -81,28 +98,22 @@ public class MainWindowController implements Initializable {
 			}
 		});
 		
-		valid.accessibleTextProperty().bind(backgroundThread.messageProperty());
+//		valid.accessibleTextProperty().bind(backgroundThread.messageProperty());
 //		expired.accessibleTextProperty().bind(backgroundThread.messageProperty());
 		
-		backgroundThread.start();
+		backgroundThread.restart();
 	}
 	
-	class MyTask extends Task<Void>{
-        
-        @Override
-        protected Void call() throws Exception {
-        	for(int i=0; i<100; i++)
-                updateMessage("cirri"+i);
-            return null;
-        }
-         
-    }
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-
+ public void cose( ObservableList<String> data){
+	 
+		Platform.runLater(new Runnable() {
+			
+			@Override
+			public void run() {
+//				System.out.println(Platform.isFxApplicationThread());
+				valid.setItems(data);
+				
+			}
+		});
+ }
 }
