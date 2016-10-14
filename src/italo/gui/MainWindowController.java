ab2a490917f8cc1102c7debbd265c611ab098c0f
Alessandro <alessandrovalori90@gmail.com>
2016-10-14 11:31:40 +0200

    trying to fix thread lock.

--- src/italo/gui/MainWindowController.java
+++ src/italo/gui/MainWindowController.java
@@ -1,6 +1,8 @@
 package italo.gui;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 
 import italo.core.GetPromo;
@@ -9,65 +11,97 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
-public class MainWindowController {
-	@FXML
-	private ListView<String> valid;
+public class MainWindowController implements Initializable {
+	@FXML private ListView<String> valid;
+	@FXML private ListView<String> expired;
+	@FXML private ListView<String> unkown;
 	private Service<Void> backgroundThread;
+	Thread myTaskThread;
+	MyTask myTask;
 	
 	public void test(){
 		
-		backgroundThread = new Service<Void>() {
-			
+//		 myTask = new MyTask();
+//		 valid.accessibleTextProperty().bind(myTask.messageProperty());
+//		 myTaskThread = new Thread(myTask);
+//         myTaskThread.start();
+         ObservableList<String> data = FXCollections.observableArrayList();	
+		backgroundThread = new Service<Void>() {	// background processing to avoid blocking the GUI			
 			@Override
 			protected Task<Void> createTask() {
-				// TODO Auto-generated method stub
-				return new Task<Void>() {
-					
+				return new Task<Void>() {				
 					@Override
 					protected Void call() throws Exception {
-						background();
+						
+						Util util = new Util();
+						GetPromo promo = new GetPromo();
+						boolean end = false;	
+						while(!end){
+							try {
+								int responce = promo.coonection(util.getUrl());
+								if(responce == 200) { //OK
+									if(promo.search()) { //promotion expire date found
+										if(util.checkExpired(promo.getData())) {
+											end = util.add();
+										} else {
+											data.add(util.getUrl());
+											valid.setItems(data);	
+											//updateMessage(data.toString()); // non so perchè funziona al posto di 
+										}
+									}
+								}
+								end = util.add();
+								TimeUnit.MILLISECONDS.sleep(200);
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								System.out.println("Connection failed");
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							
+						}
+						
 						return null;
 					}
 				};
 			}
 		};
 		
+		backgroundThread.setOnSucceeded(new EventHandler<WorkerStateEvent>() {		
+			@Override
+			public void handle(WorkerStateEvent event) {
+				valid.accessibleTextProperty().unbind();			
+			}
+		});
+		
 		valid.accessibleTextProperty().bind(backgroundThread.messageProperty());
-		backgroundThread.restart();
+//		expired.accessibleTextProperty().bind(backgroundThread.messageProperty());
+		
+		backgroundThread.start();
 	}
 	
-	private void background(){
-		ObservableList<String> data = FXCollections.observableArrayList();	
-		Util util = new Util();
-		GetPromo promo = new GetPromo();
-		boolean end = false;	
-		while(!end){
-			try {
-				int responce = promo.coonection(util.getUrl());
-				if(responce == 200) { //OK
-					if(promo.search()) { //promotion expire date found
-						if(util.checkExpired(promo.getData())) {
-							end = util.add();
-						} else {
-							data.add(util.getUrl());
-							valid.setItems(data);							
-						}
-					}
-				}
-				end = util.add();
-				TimeUnit.MILLISECONDS.sleep(200);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.out.println("Connection failed");
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		}
+	class MyTask extends Task<Void>{
+        
+        @Override
+        protected Void call() throws Exception {
+        	for(int i=0; i<100; i++)
+                updateMessage("cirri"+i);
+            return null;
+        }
+         
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 
